9dc524725076b1ae201c1ab1976d693e0f0aa908
HaseebUllah <haseebmonemac@gmail.com>
2025-12-15 21:42:07 +0500

    Update news_service.dart

--- lib/core/service/api/news_service.dart
+++ lib/core/service/api/news_service.dart
@@ -1,5 +1,5 @@
 import 'package:task_requirements/core/models/alticle.dart';
 
 abstract class NewsService {
-  Future<List<Article>?> fetchTopHeadlines();
+  Future<List<Article>?> fetchProducts();
 }
